a535cfccfa218b9741bf96d0f2ab4383782da60f
Stepanian <armikastepanian@gmail.com>
2020-04-05 17:01:12 -0400

    fixed onYes/onNo dialog buttons

--- src/app/views/add-button/add-button.component.ts
+++ src/app/views/add-button/add-button.component.ts
@@ -4,10 +4,8 @@ import {DialogComponent} from '../dialog/dialog.component';
 import {Category} from '../../model/Category';
 import {Measure} from '../../model/Measure';
 import {HttpService} from '../../service/http.service';
-import {ExercisesComponent} from '../exercises/exercises.component';
 
 export interface DialogData {
-
   title: string;
   category: Category;
   amount: number;
@@ -16,7 +14,6 @@ export interface DialogData {
   categories: Category[];
   measures: Measure[];
 }
-
 
 @Component({
   selector: 'app-add-button',
@@ -34,25 +31,18 @@ export class AddButtonComponent {
   measures: Measure[];
 
   constructor(public dialog: MatDialog,
-              private httpService: HttpService,
-              private exercisesComponent: ExercisesComponent) {
+              private httpService: HttpService) {
     this.httpService.categorySubject.subscribe(value => this.categories = value);
     this.httpService.measureSubject.subscribe(value => this.measures = value);
   }
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(DialogComponent, {
+    this.dialog.open(DialogComponent, {
       width: '250px',
       data: {
         categories: this.categories, measures: this.measures,
         title: this.title, category: this.category, amount: this.amount, measure: this.measure, date: this.date
       }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      console.log(result);
-      this.httpService.addExercise(result);
     });
   }
 }
--- src/app/views/dialog/dialog.component.ts
+++ src/app/views/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {DialogData} from '../add-button/add-button.component';
+import {HttpService} from '../../service/http.service';
 
 @Component({
   selector: 'app-dialog',
@@ -11,11 +12,20 @@ export class DialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private httpService: HttpService) {
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  onYesClick(): void {
+    this.dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+      this.httpService.addExercise(result);
+    });
   }
 
   ngOnInit(): void {
